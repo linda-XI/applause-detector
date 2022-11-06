64ec6e26269f5ad96742bb91ebedb128c202f3ac
linda-XI <yuxidai@seas.upenn.edu>
2022-11-06 17:14:46 +0000

    add main

---
+++ main.py
@@ -0,0 +1,22 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+import os
+import pathlib
+
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
+import tensorflow as tf
+
+from tensorflow.keras import layers
+from tensorflow.keras import models
+from IPython import display
+
+# Set the seed value for experiment reproducibility.
+seed = 42
+tf.random.set_seed(seed)
+np.random.seed(seed)
