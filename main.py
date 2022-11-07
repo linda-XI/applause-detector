d3be62694209a7fdd9248fca2d76f0b32b9ed2ec
linda-XI <yuxidai@seas.upenn.edu>
2022-11-07 11:36:57 +0000

    second block

--- main.py
+++ main.py
@@ -20,3 +20,13 @@ from IPython import display
 seed = 42
 tf.random.set_seed(seed)
 np.random.seed(seed)
+
+DATASET_PATH = 'data/mini_speech_commands'
+print(DATASET_PATH)
+data_dir = pathlib.Path(DATASET_PATH)
+if not data_dir.exists():
+  tf.keras.utils.get_file(
+      'mini_speech_commands.zip',
+      origin="http://storage.googleapis.com/download.tensorflow.org/data/mini_speech_commands.zip",
+      extract=True,
+      cache_dir='.', cache_subdir='data')
